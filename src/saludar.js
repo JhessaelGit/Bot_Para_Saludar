548a47f029534dd3cc944cafac6431022a91bb6d
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-08-18 15:07:25 -0400

    Saludo segun la edad agregado

--- src/saludar.js
+++ src/saludar.js
@@ -1,5 +1,22 @@
-function saludar(nombre,genero) {
-  return "hola "+ genero+" " + nombre;
+function saludar(nombre, genero, edad) {
+  let saludo;
+  genero = genero.toLowerCase();
+
+  if (edad >= 30) {
+    if (genero === "masculino") {
+      saludo = "Hola Señor " + nombre;
+    } else {
+      saludo = "Hola Señora " + nombre;
+    }
+  } else {
+    if (genero === "masculino") {
+      saludo = "Hola Joven " + nombre;
+    } else {
+      saludo = "Hola Señorita " + nombre;
+    }
+  }
+
+  return saludo;
 }
 
 export default saludar;
